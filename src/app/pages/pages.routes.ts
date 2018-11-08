95d57ac5200054fb64de29ba49e010b3c89278e9
¨Gus <agustincedeno87@gmail.com>
2018-11-07 23:54:57 -0600

    Adding Pages and Shared Modules Commit- Agustin Cedeno

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,19 @@
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+const pagesRoutes: Routes = [
+  {
+     path: '',
+     component: PagesComponent,
+     children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graficas', component: Graficas1Component },
+    ]
+  }
+];
+
+export const PAGES_ROUTES = RouterModule.forChild( pagesRoutes );
