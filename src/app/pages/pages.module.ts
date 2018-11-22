0fd6bb5dc8d018c9b0a6ac7ca4f4e788d73d69c8
¨Gus <agustincedeno87@gmail.com>
2018-11-21 23:32:05 -0600

    color change for navbar

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,6 +11,7 @@ import { PAGES_ROUTES } from './pages.routes';
 
 // Ng2 - charts
 import { ChartsModule } from 'ng2-charts';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
   declarations: [ PagesComponent,
@@ -18,7 +19,8 @@ import { ChartsModule } from 'ng2-charts';
                   ProgressComponent,
                   Graficas1Component,
                   IncrementadorComponent,
-                  GraficoDonaComponent
+                  GraficoDonaComponent,
+                  AccountSettingsComponent
                 ],
   exports:       [ DashboardComponent,
                   ProgressComponent,
